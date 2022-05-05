f7634a09004eccca07c97d69fa66f3dbd4c9307a
Patricia <waltraute@hotmail.com>
2022-05-05 22:15:19 +0200

    modificat

--- controllers/timeCtrl.js
+++ controllers/timeCtrl.js
@@ -5,15 +5,14 @@ const timeController = ((req,res)=>{
 try {
            
     let date = new Date;
-    let usuari = req.body.nom;// aquest nom es la key del json que li passo
     res.status(200).json({
       hora: `${date.getHours()}:${String(date.getMinutes()).padStart(2,'0')}`,
       data: `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
     });
 } catch (error) {
-    res.status(401).json({
+    res.status(500).json({
         success: false,
-        message: 'wrong username and password'
+        message: 'Internal Server Error'
       });
     }
 })
