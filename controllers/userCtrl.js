ac50e692e56577a6c36eebb20523dfe5f360bbad
Patricia <waltraute@hotmail.com>
2022-05-05 09:41:40 +0200

    Arreglar URL hardcodejada i treure modul requerit que no està al package

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -5,10 +5,12 @@ const config = require("../config");
 
 const userGet = (req, res) => {
   try {
+
+    let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     res.status(200).json({
       name: "Patricia",
       edat: "25",
-      url: `http://localhost:${config.port}/user`,
+     fullUrl,
     });
   } catch (error) {
     res.status(500).json({ message: `Error` });
