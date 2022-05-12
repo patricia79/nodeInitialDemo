88554e8825a9723beecc445dfb196f86f378aa20
Patricia <waltraute@hotmail.com>
2022-05-12 10:14:54 +0200

    modificat

--- db.js
+++ db.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(
   config_db.database,
   config_db.user,
   config_db.password,
+  
   {
     host: config_db.host,
     dialect: config_db.dialect,
@@ -19,7 +20,7 @@ const Player1 = PlayerMdl(sequelize, Sequelize);
 const Game1 = GameMdl(sequelize, Sequelize);
 
 sequelize
-  .sync({ force: false })
+  .sync()
   .then(() => {
     console.log("Synchronized tables");
   })
@@ -27,14 +28,6 @@ sequelize
     console.log("Has occurred an error", error);
   });
 
-sequelize
-  .drop()
-  .then(() => {
-    console.log("Deleted tables");
-  })
-  .catch((error) => {
-    console.log("Has occurred an error", error);
-  });
 
 module.exports = {
   Player1, Game1
