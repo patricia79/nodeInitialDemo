1812c13825d8149ca56f436345acd316b37d18c6
Patricia <waltraute@hotmail.com>
2022-05-17 00:37:10 +0200

    amb aquestes rutes funciona

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const app = express()
+const jocAPI = require('./routes/router')
 require('./connectionDB')
-const config_db = require('./config')
-
-const router = express.Router()
-const jocApi = express.Router()
+const configDB = require('./config')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(router)
-app.use(jocApi)
+app.use(jocAPI)
 
-app.listen(config_db.port, () => {
-  console.log(`API REST en http://localhost:${config_db.port}/`);
+app.listen(configDB.port, () => {
+  console.log(`API REST en http://localhost:${configDB.port}/`);
 });
