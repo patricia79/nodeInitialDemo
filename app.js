fa8a879a1c2cb2329b2b3859f6ab02d74e1307b6
Patricia <waltraute@hotmail.com>
2022-05-17 00:43:56 +0200

    rutes funcionant ok

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const jocAPI = require('./routes/router')
+const router = require('./routes/router')
 require('./connectionDB')
 const configDB = require('./config')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(jocAPI)
+
+app.use(router)
 
 app.listen(configDB.port, () => {
   console.log(`API REST en http://localhost:${configDB.port}/`);
--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const jocAPI = express.Router()
 const router = express.Router()
 
 const { 
@@ -17,14 +16,14 @@ const {
 
   
 
-jocAPI.post('/players', addPlayer) //POST  crea un jugador
-jocAPI.post('/players/:id/games', playerRollDices) //POST /players/{id}/games: un jugador específic realitza una tirada --Works
-jocAPI.put('/players/:id', updatePlayer) //PUT  modifica el nom del jugador --Works
-jocAPI.delete('/players/:id/games', deleteGames) // DELETE /players/{id}/games: elimina les tirades del jugador --Works
-jocAPI.get('/players', getAllPlayers)//GET : retorna el llistat de tots els jugadors del sistema amb el seu percentatge mig d’èxits --Works
-jocAPI.get('/players/:id/games', playerGamesList)//GET /players/{id}/games: retorna el llistat de jugades per un jugador. --Works
-jocAPI.get('/players/ranking', generalRanking) //GET : retorna el percentatge mig d’èxits del conjunt de tots els jugadors --Works
-jocAPI.get('/players/ranking/loser', worstPlayer )//GET : retorna el jugador amb pitjor percentatge d’èxit --Works
-jocAPI.get('/players/ranking/winner', betterPlayer)//GET : retorna el jugador amb millor percentatge d’èxit  --Works
+router.post('/players', addPlayer) //POST  crea un jugador
+router.post('/players/:id/games', playerRollDices) //POST /players/{id}/games: un jugador específic realitza una tirada --Works
+router.put('/players/:id', updatePlayer) //PUT  modifica el nom del jugador --Works
+router.delete('/players/:id/games', deleteGames) // DELETE /players/{id}/games: elimina les tirades del jugador --Works
+router.get('/players', getAllPlayers)//GET : retorna el llistat de tots els jugadors del sistema amb el seu percentatge mig d’èxits --Works
+router.get('/players/:id/games', playerGamesList)//GET /players/{id}/games: retorna el llistat de jugades per un jugador. --Works
+router.get('/players/ranking', generalRanking) //GET : retorna el percentatge mig d’èxits del conjunt de tots els jugadors --Works
+router.get('/players/ranking/loser', worstPlayer )//GET : retorna el jugador amb pitjor percentatge d’èxit --Works
+router.get('/players/ranking/winner', betterPlayer)//GET : retorna el jugador amb millor percentatge d’èxit  --Works
 
-module.exports = jocAPI, router
+module.exports = router
